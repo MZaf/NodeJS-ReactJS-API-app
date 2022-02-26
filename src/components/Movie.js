9ab43bbc960a982eb0b5ec66b64c6f72fb787ee4
mzaf <acer.bwp@gmail.com>
2022-02-26 23:26:11 +0500

    Add section Top 10 Movies/Rating D3 Stat page

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -66,7 +66,7 @@ const Movie = observer(class extends Component {
 
                 </div>
                 <div className="rec-grid">
-                    <span className="title">Top 10 Movies/Rating D3 Stat page</span>
+                    <span className="title">Top 10 Movies/Rating D3 Stat page </span>
                     
                     <span className="title">Similar Movies</span>
                     {results ? results.slice(0, 6).map(({id, 
